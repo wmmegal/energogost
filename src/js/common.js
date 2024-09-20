a5a2ff2bc403f7da6ed33898db6eb3ca3a2abe34
wmmeg <wmmegal@gmail.com>
2024-09-20 16:50:50 +0300

    feat: all done

--- src/js/common.js
+++ src/js/common.js
@@ -1,3 +1,27 @@
-import 'virtual:svg-icons-register'
-import '../css/tailwind.css'
+import 'virtual:svg-icons-register';
+import '../css/tailwind.css';
+import '@splidejs/splide/css/core';
+import Splide from '@splidejs/splide';
+import Alpine from 'alpinejs';
+import { Fancybox } from '@fancyapps/ui';
+import '@fancyapps/ui/dist/fancybox/fancybox.css';
 
+window.Alpine = Alpine;
+
+Alpine.start();
+
+new Splide('.splide', {
+    direction: 'ttb',
+    heightRatio: 0.3,
+    arrows: false,
+    pagination: true,
+    autoplay: true,
+    interval: 5000,
+    breakpoints: {
+        640: {
+            heightRatio: 0.5,
+        },
+    },
+}).mount();
+
+Fancybox.bind('[data-fancybox]');
